77c07172ef968f72c50d71361ee4948fa8dca03b
Rreuben <jreuben07@gmail.com>
2018-05-11 17:12:30 +0300

    Complete css and part of user interface

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 var player1 = '';
 var player2 = '';
 
+var throwDice = () => Math.floor( 6 * Math.random()) + 1;
 
 
 //User Interface
@@ -10,11 +11,19 @@ $(document).ready(function() {
   $('button#startBtn').click(function(event) {
     $('.startMenu').hide();
     $('.playerConsole').show();
+
+    var player1Name = $('.player1Name').val();
+    $("#player1Name").text(player1Name);
+
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+
   });
 
   $('button#newGameBtn').click(function(event) {
     $('.startMenu').show();
     $('.playerConsole').hide();
   });
+
 
 });
